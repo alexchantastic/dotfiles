d6b62598d88e6453e36bb47cc369d76177b0d99f
Alex Chan <alexchantastic@users.noreply.github.com>
2024-03-12 11:48:41 -0700

    disable treesitter on vscode

--- nvim.lua
+++ nvim.lua
@@ -74,16 +74,18 @@ require("catppuccin").setup({
 })
 
 -- Plugin: nvim-treesitter
-require'nvim-treesitter.configs'.setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = { "javascript" },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-})
+if not vim.g.vscode then
+  require'nvim-treesitter.configs'.setup({
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = { "javascript" },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+  })
+end
 
 -- Plugin: fzf-lua
 require("fzf-lua").setup({})
