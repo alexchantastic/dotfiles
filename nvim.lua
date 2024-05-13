c1872ac04c166bcd0b9000cf33773771ca5b0138
Alex Chan <alexchantastic@users.noreply.github.com>
2024-05-13 10:54:27 -0700

    refactor plugin structure

--- nvim.lua
+++ nvim.lua
@@ -1,3 +1,48 @@
+-- Behaviors
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.showmatch = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.fillchars = { eob = " " }
+vim.opt.clipboard = "unnamedplus"
+vim.opt.inccommand = "nosplit"
+vim.opt.shortmess = "I"
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true
+vim.opt.si = true
+vim.opt.wrap = true
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = "rounded" }
+)
+
+-- Keybinds
+vim.keymap.set({ "n", "x" }, "d", "\"_d")
+vim.keymap.set({ "n", "x" }, "D", "\"_D")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Commands
+vim.cmd("command! Format lua vim.lsp.buf.format()<CR>")
+
 -- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -19,20 +64,63 @@ require("lazy").setup({
     cond = not vim.g.vscode,
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    cond = not vim.g.vscode,
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {},
   },
   {
-    "williamboman/mason.nvim",
+    "lewis6991/gitsigns.nvim",
     cond = not vim.g.vscode,
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    }
+    opts = {},
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+  },
+  {
+    "ibhagwan/fzf-lua",
+    cond = not vim.g.vscode,
+    init = function()
+      vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+      vim.cmd("command! Ff FzfLua files")
+      vim.cmd("command! Fb FzfLua buffers")
+      vim.cmd("command! Fg FzfLua lgrep_curbuf")
+      vim.cmd("command! Fgg FzfLua live_grep")
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     cond = not vim.g.vscode,
+    dependencies = {
+      {
+        'williamboman/mason.nvim',
+        config = true,
+      },
+      "williamboman/mason-lspconfig.nvim",
+    },
+    init = function()
+      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+    end,
+    config = function()
+      require("mason").setup()
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        automatic_installation = true,
+      })
+      mason_lspconfig.setup_handlers({
+        function (server_name)
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+            })
+        end
+      })
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
@@ -42,64 +130,247 @@ require("lazy").setup({
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-    }
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          "saadparwaiz1/cmp_luasnip",
+        }
+      },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        view = {
+          docs = {
+            auto_open = true,
+          },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+      
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          { name = "cmdline" }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+    end
   },
   {
-    "L3MON4D3/LuaSnip",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     cond = not vim.g.vscode,
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-    }
+    opts = {
+      flavour = "mocha",
+      integrations = {
+        sandwich = true,
+        which_key = true,
+        barbar = true,
+        mason = true,
+        lsp_trouble = true,
+        indent_blankline = {
+          enabled = true,
+          scope_color = "overlay0",
+        },
+      },
+    },
+    init = function()
+      vim.cmd.colorscheme("catppuccin")
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    cond = not vim.g.vscode,
+    opts = {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = { "javascript" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = "+",
+          node_decremental = "_",
+        },
+      },
+    },
   },
   {
     "nvimtools/none-ls.nvim",
     cond = not vim.g.vscode,
-  },
-  {
-    "ibhagwan/fzf-lua",
-    cond = not vim.g.vscode,
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.prettierd,
+        },
+      })
+    end
   },
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    ---@type Flash.Config
     opts = {},
-    -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
   "machakann/vim-sandwich",
-  { "numToStr/Comment.nvim", lazy = false },
   "mg979/vim-visual-multi",
-  "gbprod/yanky.nvim",
+  {
+    "gbprod/yanky.nvim",
+    opts = {},
+    init = function()
+      vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+      vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+      vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+      vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+      vim.keymap.set("n", "<c-j>", "<Plug>(YankyPreviousEntry)")
+      vim.keymap.set("n", "<c-k>", "<Plug>(YankyNextEntry)")
+      vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+      vim.cmd("command! Y YankyRingHistory")
+    end,
+  },
   {
     "nvim-tree/nvim-tree.lua",
     lazy = false,
     cond = not vim.g.vscode,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    cond = not vim.g.vscode,
+    opts = {
+      filters = { custom = { "^.git$" } },
+      renderer = {
+        icons = {
+          show = {
+            file = false,
+            folder = false,
+            git = false,
+          },
+          glyphs = {
+            folder = {
+              arrow_open = "▾",
+              arrow_closed = "▸",
+              default = "▸",
+              open =  "▾",
+              empty = "▸",
+              empty_open = "▾",
+              symlink = "▸",
+              symlink_open = "▾",
+            },
+          },
+        },
+      },
+    },
+    init = function()
+      vim.cmd("command! T NvimTreeToggle")
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
     cond = not vim.g.vscode,
+    opts = {
+      options = {
+        icons_enabled = false,
+        component_separators = { left = "", right = ""},
+        section_separators = { left = "", right = ""},
+        theme = "catppuccin",
+      },
+    },
   },
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     cond = not vim.g.vscode,
+    opts = {
+      indent = {
+        char = "│",
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    },
   },
   {
     "folke/trouble.nvim",
     branch = "dev",
     cond = not vim.g.vscode,
+    opts = {},
+    init = function()
+      vim.cmd("command! Tr Trouble diagnostics toggle")
+    end,
   },
   {
     "romgrk/barbar.nvim",
-    init = function() vim.g.barbar_auto_setup = false end,
+    cond = not vim.g.vscode,
     opts = {
       auto_hide = 1,
       icons = {
@@ -109,7 +380,11 @@ require("lazy").setup({
         },
       },
     },
-    cond = not vim.g.vscode,
+    init = function()
+      vim.g.barbar_auto_setup = false
+      vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+    end,
   },
   {
     "utilyre/barbecue.nvim",
@@ -118,319 +393,35 @@ require("lazy").setup({
       "SmiteshP/nvim-navic",
     },
     cond = not vim.g.vscode,
+    opts = {
+      symbols = {
+        separator = "/",
+      },
+      kinds = false,
+    },
   },
   {
     "gennaro-tedesco/nvim-possession",
-    config = true,
     cond = not vim.g.vscode,
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
+    opts = {
+      autoload = true,
+      autoswitch = {
+        enable = true,
+      },
+      fzf_winopts = {
+        height = 0.85,
+        width = 0.80,
+        preview = {
+          horizontal = "right:60%",
+        },
+      },
+    },
     init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    cond = not vim.g.vscode,
+      vim.cmd("command! Sl lua require('nvim-possession').list()<CR>")
+      vim.cmd("command! Fs lua require('nvim-possession').list()<CR>")
+      vim.cmd("command! Sn lua require('nvim-possession').new()<CR>")
+      vim.cmd("command! Su lua require('nvim-possession').update()<CR>")
+      vim.cmd("command! Sd lua require('nvim-possession').delete()<CR>")
+    end,
   },
 })
-
--- Behaviors
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.showmatch = true
-vim.opt.mouse = "a"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.fillchars = { eob = " " }
-vim.opt.clipboard = "unnamedplus"
-vim.opt.inccommand = "nosplit"
-vim.opt.shortmess = "I"
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover,
-  { border = "rounded" }
-)
-
--- Colors
-vim.opt.termguicolors = true
-
--- Text
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.ai = true
-vim.opt.si = true
-vim.opt.wrap = true
-
--- Theme
-if not vim.g.vscode then
-  require("catppuccin").setup({
-    flavour = "mocha",
-    integrations = {
-      sandwich = true,
-      which_key = true,
-      barbar = true,
-      mason = true,
-      lsp_trouble = true,
-      indent_blankline = {
-        enabled = true,
-        scope_color = "overlay0",
-      },
-    },
-  })
-
-  vim.cmd.colorscheme("catppuccin")
-end
-
--- Keybinds
-vim.keymap.set({ "n", "x" }, "d", "\"_d")
-vim.keymap.set({ "n", "x" }, "D", "\"_D")
-vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-
--- Commands
-vim.cmd("command! Format lua vim.lsp.buf.format()<CR>")
-
--- Plugins
-if not vim.g.vscode then
-  -- Plugin: nvim-treesitter
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown_inline" },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = { "javascript" },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        node_incremental = "+",
-        node_decremental = "_",
-      },
-    },
-  })
-  
-  -- Plugin: mason.nvim
-  require("mason").setup()
-  local mason_lspconfig = require("mason-lspconfig")
-  mason_lspconfig.setup({
-    automatic_installation = true,
-  })
-  mason_lspconfig.setup_handlers({
-    function (server_name)
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        require("lspconfig")[server_name].setup({
-          capabilities = capabilities,
-        })
-    end
-  })
-
-  -- Plugin: nvim-cmp
-  local cmp = require("cmp")
-  local luasnip = require("luasnip")
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    view = {
-      docs = {
-        auto_open = true,
-      },
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          if luasnip.expandable() then
-            luasnip.expand()
-          else
-            cmp.confirm({
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            })
-          end
-        else
-          fallback()
-        end
-      end),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.locally_jumpable(1) then
-          luasnip.jump(1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-    }, {
-      { name = "buffer" },
-    })
-  })
-  
-  cmp.setup.cmdline({ "/", "?" }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" }
-    }
-  })
-
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" }
-    }, {
-      { name = "cmdline" }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
-  
-  -- Plugin: none-ls.nvim
-  local null_ls = require("null-ls")
-  null_ls.setup({
-    sources = {
-      null_ls.builtins.formatting.prettierd,
-    },
-  })
-
-  -- Plugin: fzf-lua
-  require("fzf-lua").setup({})
-  vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-  vim.cmd("command! Ff FzfLua files")
-  vim.cmd("command! Fb FzfLua buffers")
-  vim.cmd("command! Fg FzfLua lgrep_curbuf")
-  vim.cmd("command! Fgg FzfLua live_grep")
-
-  -- Plugin: lualine.nvim
-  require("lualine").setup({
-    options = {
-      icons_enabled = false,
-      component_separators = { left = "", right = ""},
-      section_separators = { left = "", right = ""},
-      theme = "catppuccin",
-    },
-  })
-  
-  -- Plugin: barbar.nvim
-  vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
-  vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
-  
-  -- Plugin: barbecue.nvim
-  require("barbecue").setup({
-    symbols = {
-      separator = "/",
-    },
-    kinds = false,
-  })
-
-  -- Plugin: indent-blankline.nvim
-  require("ibl").setup({
-    indent = {
-      char = "│",
-    },
-    scope = {
-      show_start = false,
-      show_end = false,
-    },
-  })
-  
-  -- Plugin: gitsigns.nvim
-  require("gitsigns").setup()
-
-  -- Plugin: nvim-possession
-  require("nvim-possession").setup({
-    autoload = true,
-    autoswitch = {
-      enable = true,
-    },
-    fzf_winopts = {
-      height = 0.85,
-      width = 0.80,
-      preview = {
-        horizontal = "right:60%",
-      },
-    },
-  })
-
-  vim.cmd("command! Sl lua require('nvim-possession').list()<CR>")
-  vim.cmd("command! Fs lua require('nvim-possession').list()<CR>")
-  vim.cmd("command! Sn lua require('nvim-possession').new()<CR>")
-  vim.cmd("command! Su lua require('nvim-possession').update()<CR>")
-  vim.cmd("command! Sd lua require('nvim-possession').delete()<CR>")
- 
-  -- Plugin: trouble.nvim
-  require("trouble").setup()
-  vim.cmd("command! Tr Trouble diagnostics toggle")
-
-  -- Plugin: nvim-tree
-  require("nvim-tree").setup({
-    filters = { custom = { "^.git$" } },
-    renderer = {
-      icons = {
-        show = {
-          file = false,
-          folder = false,
-          git = false,
-        },
-        glyphs = {
-          folder = {
-            arrow_open = "▾",
-            arrow_closed = "▸",
-            default = "▸",
-            open =  "▾",
-            empty = "▸",
-            empty_open = "▾",
-            symlink = "▸",
-            symlink_open = "▾",
-          },
-        },
-      },
-    },
-  })
-  vim.cmd("command! T NvimTreeToggle")
-end
-
--- Plugin: yanky.nvim
-require("yanky").setup({})
-vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-vim.keymap.set("n", "<c-j>", "<Plug>(YankyPreviousEntry)")
-vim.keymap.set("n", "<c-k>", "<Plug>(YankyNextEntry)")
-vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
-vim.cmd("command! Y YankyRingHistory")
-
--- Plugin: comment.nvim
-require("Comment").setup()
