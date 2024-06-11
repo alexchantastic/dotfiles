52e8643cd03e1164b4f31f00127c921824bb407c
Alex Chan <alexchantastic@users.noreply.github.com>
2024-06-11 11:54:20 -0700

    add comment.nvim

--- nvim.lua
+++ nvim.lua
@@ -698,6 +698,23 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"numToStr/Comment.nvim",
+		event = "BufReadPost",
+		dependencies = {
+			{
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				opts = {
+					enable_autocmd = false,
+				},
+			},
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
+	{
 		"kylechui/nvim-surround",
 		event = "BufReadPost",
 		opts = {},
