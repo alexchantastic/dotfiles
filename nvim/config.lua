32d28d8e35c721ac2ca444eb8a1106bf5d5b8d60
Alex Chan <alexchantastic@gmail.com>
2024-12-14 15:56:05 -0800

    add treewalker.nvim

--- nvim/config.lua
+++ nvim/config.lua
@@ -848,6 +848,16 @@ require("lazy").setup({
 		event = { "CursorMoved" },
 		opts = {},
 	},
+	{
+		"aaronik/treewalker.nvim",
+		event = { "BufReadPost" },
+		config = function()
+			vim.api.nvim_set_keymap("n", "<C-j>", ":Treewalker Down<CR>", { noremap = true })
+			vim.api.nvim_set_keymap("n", "<C-k>", ":Treewalker Up<CR>", { noremap = true })
+			vim.api.nvim_set_keymap("n", "<C-h>", ":Treewalker Left<CR>", { noremap = true })
+			vim.api.nvim_set_keymap("n", "<C-l>", ":Treewalker Right<CR>", { noremap = true })
+		end,
+	},
 }, {
 	install = {
 		colorscheme = { "catppuccin" },
